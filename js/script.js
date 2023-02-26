72415d69f3b989a5f446793131c8af37e50cd0a2
Vitalii Palkin <vitalijpalkin@yandex.ru>
2023-02-26 13:28:46 +0300

    dorabotka

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//const swiper = new Swiper('.new__slider'), {
+//  loop: true,
+//	slidesPerView: 1, 
+//  pagination: {
+//    el: '.swiper-pagination',
+//  },
+//});
+
+//const swiper = new Swiper(...)
